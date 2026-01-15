3bf7efc5baffb3b93de3759e3bca0482147dba5d
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-14 22:04:26 -0300

    Add Lua.js for converting code to Lua syntax

---
+++ src/funkin/Lua.js
@@ -0,0 +1,8 @@
+export function toLua(code) {
+  return code
+    .replace(/func/g, "function")
+    .replace(/let |var |const /g, "")
+    .replace(/{/g, "")
+    .replace(/}/g, "end")
+    .replace(/print/g, "debugPrint");
+}
